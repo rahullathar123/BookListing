cb62070b7a4609469d88ac14789fede03e73b749
rahullathar123 <rahullathar123@gmail.com>
2017-09-04 03:42:05 -0400

    commit2

--- app/src/main/java/com/example/rahul/booklisting/Utils.java
+++ app/src/main/java/com/example/rahul/booklisting/Utils.java
@@ -2,11 +2,9 @@ package com.example.rahul.booklisting;
 
 import android.text.TextUtils;
 import android.util.Log;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
